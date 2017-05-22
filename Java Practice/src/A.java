610e480a7ad7ade67211e718e28d8891e91f0690
Ankit Gupta <ankitgupta09101987@gmail.com>
2017-05-21 21:25:50 -0700

    If class B extends class A and override a method. Can the override method in B not throw the exception which was thrown in A?

---
+++ Java Practice/src/A.java
@@ -0,0 +1,18 @@
+/**
+ * Created by ankitgupta on 5/21/17.
+ */
+public class A {
+
+    public void hello(){
+        try {
+            System.out.println(10/0);
+        }catch (ArithmeticException e)
+        {
+            System.out.println(e);
+        }
+        finally {
+            System.out.println("Hello");
+        }
+
+    }
+}
---
+++ Java Practice/src/B.java
@@ -0,0 +1,17 @@
+/**
+ * Created by ankitgupta on 5/21/17.
+ */
+
+public class B extends A{
+
+    public void hello(){
+        System.out.println(10/3);
+    }
+
+    public static void main(String[] args){
+
+        B b = new B();
+
+        b.hello();
+    }
+}
